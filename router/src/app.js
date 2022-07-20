60f4e2238ddcd5502ba065e5a7adae95f4ca92ee
Diego Garcia Sanchez <diego.garciasanchez@gmail.com>
2022-07-20 04:50:21 +0200

    desafio router

---
+++ router/src/app.js
@@ -0,0 +1,20 @@
+import expres from "express";
+import { json } from "express";
+import productsRouter from "./routes/products.router.js";
+
+const app = expres();
+app.use(expres.urlencoded({ extended: true }))
+
+//static
+app.use(expres.static('public'));
+app.use(expres.json());
+ 
+
+//routes
+app.use('/api/productos', productsRouter)
+
+// inicio el servidor
+const PORT = 8080;
+const connectedServer = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
---
+++ router/src/routes/products.router.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import Contenedor from "../manager/Contenedors.js";
+
+const router = Router();
+
+const contenedor = new Contenedor('../src/productos.json')
+
+router.get('/', async (req,res) => {
+    
+    let prods =  await contenedor.getAll().then(x=>x)
+    res.send(prods)
+})
+
+router.get('/:id', async (req,res) => {
+    const id = parseInt(req.params.id) 
+    let prods =  await contenedor.getById(id).then(x=>x)
+    res.send(prods)
+})
+
+router.post('/', async (req,res) => {
+    
+    const product = req.body 
+    let ids =  await contenedor.save(product).then(id=>id);
+    res.send({...product, id: ids} )
+})
+
+router.put('/:id', async (req,res) => {
+    const id = parseInt(req.params.id) 
+    const newProduct = req.body
+    await contenedor.updateById(id,newProduct)
+    res.send();
+})
+
+router.delete('/:id', async (req,res) => {
+    const id = parseInt(req.params.id) 
+    await contenedor.deleteById(id)
+    res.send();
+})
+
+export default router;
