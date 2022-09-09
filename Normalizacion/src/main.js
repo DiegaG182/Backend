b3fd1536eda1558e1432ff5b786946eba98ffff1
Diego Garcia Sanchez <diego.garciasanchez@gmail.com>
2022-09-09 04:35:46 +0200

    Desafio Mocks ayuda

--- Normalizacion/src/main.js
+++ Normalizacion/src/main.js
@@ -47,12 +47,13 @@ app.use(express.static('../public'));
 import ContenedorMongoDB from '../contenedores/ContenedorMongo.js';
 import { json } from 'stream/consumers';
 //probe declarar aca tambien tanto el schema como el model, xq  no me toma el populate
-const messagesSchema = new mongoose.Schema(config.mongoDB.schema.message, { timestamps: true })
+/* const messagesSchema = new mongoose.Schema(config.mongoDB.schema.message, { timestamps: true })
 const authorsSchema = new mongoose.Schema(config.mongoDB.schema.author, { timestamps: true })
 const messages = mongoose.model('messages',messagesSchema)
 const authors = mongoose.model('authors',authorsSchema)
-const chatApi = new ContenedorMongoDB('messages',messagesSchema);
-const authorApi = new ContenedorMongoDB('authors',authorsSchema);
+ */
+const chatApi = new ContenedorMongoDB('messages',config.mongoDB.schema.message);
+const authorApi = new ContenedorMongoDB('authors',config.mongoDB.schema.author);
 const productosApi = new ContenedorMongoDB('products',config.mongoDB.schema.products);
 
 
@@ -107,32 +108,52 @@ io.on('connection', async socket => {
     socket.on("new-message",async message =>{
         
 
-        //const autor = await authorApi.save(message.author)
-        //console.log(autor)
-        /* const findAuthor = await authorApi.getByMail(message.author.mail)
-        let newId;  
+    /*     const findAuthor = await authorApi.getByMail(message.author.mail)
+          
         if (findAuthor == 0){
             findAuthor = await authorApi.save(message.author)
         }
 
         const chat = await chatApi.save({author: findAuthor._id, message: message.message})
          */
-        //let schemaAuthor =  authorApi.collection.schema
+        let authorModel =  authorApi.collection
         //console.log(schemaAuthor)
-        let populate = await chatApi.collection.findById({_id: "631a8fa29ea5d7bdaa3d5e4b"}).populate("authors")
-        //let populated = await chatApi.getPopulated(/* findAuthor._id */ "631a8fa29ea5d7bdaa3d5e4b","authors")
-        //console.log(populate)
+        //let populate = await chatApi.collection.findById({_id: "631a8fa29ea5d7bdaa3d5e4b"}).populate("authors")
+        let allChats = await chatApi.collection.find({}).populate("authorModel")
+        console.log(allChats)
           
-        /* const authorN = new schema.Entity('authors')
+        const authorN = new schema.Entity('authors')
         const messageN = new schema.Entity('messages',{
             author:authorN
         })
         const normalizedData = normalize(allChats,[messageN])
-        console.log(JSON.stringify(normalizedData, null, '/t')) */
+        console.log(JSON.stringify(normalizedData, null, '/t'))  
 
-
-
+        //********************************************
+        /* no me toma el modelo, ni importandolo como hice aca, ni definiendolo arriba, como esta comentado, no se que hacer
         
+        C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\helpers\populate\getModelsMapForPopulate.js:48
+    return new StrictPopulate(options._fullPath || options.path);
+           ^
+
+StrictPopulateError: Cannot populate path `authorModel` because it is not in your schema. Set the `strictPopulate` option to false to override.
+    at getModelsMapForPopulate (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\helpers\populate\getModelsMapForPopulate.js:48:12)
+    at populate (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\model.js:4637:21)
+    at _populate (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\model.js:4597:5)
+    at C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\model.js:4574:5
+    at promiseOrCallback (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\helpers\promiseOrCallback.js:11:14)
+    at Mongoose._promiseOrCallback (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\index.js:1233:10)
+    at Function.Model.populate (C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\model.js:4572:23)
+    at C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\query.js:2289:19
+    at C:\coderhouse\backend\2proyecto\Normalizacion\node_modules\mongoose\lib\helpers\query\completeMany.js:37:32      
+    at processTicksAndRejections (node:internal/process/task_queues:78:11) {
+  path: 'authorModel'
+}
+
+
+
+
+        */
         //console.log(result)
         //const mensajes = JSON.parse(JSON.stringify(await mensajesApi.getAll().then(mensajes => mensajes)))
         
